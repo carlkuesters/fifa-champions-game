031e8adbd2a0ad0a55947662d49edfe0f8bd541c
carlkuesters <carl.kuesters@youengineering.com>
2020-10-30 21:25:13 +0100

    Fixed DontLoop animations not freezing properly at end.

--- src/main/java/com/carlkuesters/fifachampions/game/PlayerAnimation.java
+++ src/main/java/com/carlkuesters/fifachampions/game/PlayerAnimation.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.carlkuesters.fifachampions.game;
 
 import com.jme3.animation.LoopMode;
 
-/**
- *
- * @author Carl
- */
 public class PlayerAnimation implements GameLoopListener {
 
     public PlayerAnimation(String name, float loopDuration) {
@@ -46,6 +37,6 @@ public class PlayerAnimation implements GameLoopListener {
     }
 
     public boolean isFinished() {
-        return (remainingDuration <= 0);
+        return ((remainingDuration <= 0) && (loopMode != LoopMode.DontLoop));
     }
 }
