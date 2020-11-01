5bb5205995d466a6682592645fbe7ab7c7bfed2d
carlkuesters <carl.kuesters@youengineering.com>
2020-11-01 01:16:25 +0100

    Improved charged strength for free kicks.

--- src/main/java/com/carlkuesters/fifachampions/game/buttons/behaviours/NearFreeKickShootButtonBehaviour.java
+++ src/main/java/com/carlkuesters/fifachampions/game/buttons/behaviours/NearFreeKickShootButtonBehaviour.java
@@ -3,23 +3,25 @@ package com.carlkuesters.fifachampions.game.buttons.behaviours;
 import com.carlkuesters.fifachampions.game.Ball;
 import com.carlkuesters.fifachampions.game.PlayerObject;
 import com.carlkuesters.fifachampions.game.situations.NearFreeKickSituation;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 
 public class NearFreeKickShootButtonBehaviour extends ApproachShootButtonBehaviour<NearFreeKickSituation> {
 
     @Override
     protected void shoot(NearFreeKickSituation nearFreeKickSituation, PlayerObject playerObject, float strength) {
+        float optimalStrength = nearFreeKickSituation.getOptimalShootStrength();
         // TODO: Player dependent
         float maximumShootVelocity = 29;
-        float optimalBallVelocityLength = (nearFreeKickSituation.getOptimalShootStrength() * maximumShootVelocity);
         Ball ball = playerObject.getGame().getBall();
         float optimalBallVelocityX = nearFreeKickSituation.getTargetInGoalPosition()
                 .subtract(ball.getPosition())
                 .normalizeLocal()
-                .multLocal(optimalBallVelocityLength)
+                .multLocal(optimalStrength * maximumShootVelocity)
                 .getX();
         Vector3f optimalBallVelocity = ball.getInitialVelocity_ByTargetVelocityX(nearFreeKickSituation.getTargetInGoalPosition(), optimalBallVelocityX);
-        Vector3f actualBallVelocity = optimalBallVelocity.normalize().multLocal(strength * maximumShootVelocity);
+        float actualVelocityFactor = FastMath.pow(2, strength - optimalStrength);
+        Vector3f actualBallVelocity = optimalBallVelocity.normalize().multLocal(actualVelocityFactor * maximumShootVelocity);
         playerObject.shoot(actualBallVelocity);
     }
 }
