145b52ad0828a32ce1fc9b1f1bdad0f0a85b76af
carlkuesters <carl.kuesters@youengineering.com>
2022-09-17 22:48:45 +0200

    Added initial Tobi face texture.

--- src/main/java/com/carlkuesters/fifachampions/visuals/PlayerSkins.java
+++ src/main/java/com/carlkuesters/fifachampions/visuals/PlayerSkins.java
@@ -35,7 +35,7 @@ public class PlayerSkins {
         skins.put(Players.MIRCO, new PlayerSkin("mirco", "general_2", new ColorRGBA(0.6f, 0.33f, 0.13f, 1)));
         skins.put(Players.SCHLATTI, new PlayerSkin("schlatti", "general_1", new ColorRGBA(0.48f, 0.31f, 0.1f, 1)));
         skins.put(Players.FRIEDRICH, new PlayerSkin("friedrich", "general_1", new ColorRGBA(0.18f, 0.11f, 0.07f, 1)));
-        skins.put(Players.TOBI, new PlayerSkin("default", null, null));
+        skins.put(Players.TOBI, new PlayerSkin("tobi", null, null));
         skins.put(Players.CHRIS, new PlayerSkin("chris", null, null));
         skins.put(Players.FABIAN_BUNGUS, new PlayerSkin("fabian_bungus", "general_1", new ColorRGBA(0.5f, 0.33f, 0.1f, 1)));
         skins.put(Players.MIMI, new PlayerSkin("default", null, null));
